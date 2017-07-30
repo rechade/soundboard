cf7b879c3666b5d7cf78c6ee1fe961be1db74d95
Richie Gusto <richie.gusto@gmail.com>
2017-07-30 07:18:41 +0100

    simplified sound generator. removed audiosynth and touchmove, disabled default gestures, works multitouch or with a mouse (and touch stops mouse events to prevent duplicating notes) - TODO: scale to screen, look into enveloping, GUI for changing scale / layout / rootnote

--- scripts/pads.js
+++ scripts/pads.js
@@ -33,50 +33,13 @@ var pads = {
         y = canvasHeight - pads.padHeight;                  
         for (row=0; row<pads.NUM_ROWS; row++) {                  
             for (col=0; col<pads.NUM_COLS; col++){
-                //var rect = [x,y,pads.padWidth,pads.padHeight];
-                //padRects.push(rect);
                 x=col*pads.padWidth;                
                 pads.drawPad(ctx,pads.padPaints[i],x,y,pads.padWidth, pads.padHeight);
-                // next column                
-                //x+= pads.padWidth;                
+                // next column                                
                 i++;
             }
             // next row
             y-=pads.padHeight;            
         }                
-    },
-
-	allocatePadPaints : function() {
-		var i=0;
-        var padPaintArrayIndex=0;
-        var lastRowStartedAt=0;
-        while (padPaintArrayIndex < pads.NUM_PADS) {
-            i = i%12;
-            if (i==0) {
-                pads.padPaints.push(pads.rootPaint);
-				padPaintArrayIndex++;
-            } else if (scales.scaleArray[i]) {
-                pads.padPaints.push(pads.whitePaint);
-				padPaintArrayIndex++;
-            } else if (!scales.inKey) {
-                pads.padPaints.push(pads.blackPaint);
-				padPaintArrayIndex++;
-            }
-            // we always move to next semitone in the scale
-            i++;
-            // did we just pass the end of a row
-            if ((padPaintArrayIndex%pads.NUM_COLS)==0) {
-                i = lastRowStartedAt + scales.semitonesToNextRow(lastRowStartedAt);
-                i = i%12;
-                // if we're in key and the jump to the next row would land on a non key note,
-                // skip to next key note
-                while (scales.inKey & !scales.scaleArray[i]) {
-                    i++;
-                    i = i%12;
-                }
-                // save square 1 info for next jump
-                lastRowStartedAt = i;
-            }
-        }
-	},
+    },	
 };
